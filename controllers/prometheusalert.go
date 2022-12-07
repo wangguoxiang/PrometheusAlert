18fb7e04130400d3e506fabd9306da9dd203284c
Your Name <you@example.com>
2022-12-07 16:05:52 +0800

    update test6

--- controllers/prometheusalert.go
+++ controllers/prometheusalert.go
@@ -430,7 +430,11 @@ func TransformAlertMessage(p_json interface{}, tpltext string) (error error, msg
 	case string:
 		fmt.Println("string:", v)
 	case []string:
-		fmt.Println("[]string:", &v)
+		fmt.Println("[]string:", v)
+	case []byte:
+		fmt.Println("[]byte:", v)
+    case []interface:
+    	fmt.Println("[]interface:", v)
 	default:
 		fmt.Println("unknown type:", v)
 	}
